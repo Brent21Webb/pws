c66a2b67164a782839740560abb4234ee6f39c37
Brent21Webb <brent20020@gmail.com>
2019-09-08 13:08:43 +0200

    Added HMTL element to constructor, initialise function

--- js/class.canvas.js
+++ js/class.canvas.js
@@ -12,5 +12,7 @@ class Canvas {
 		this.ctx = this.canvas.getContext("2d");
 		this.ctx.width = this.canvas.width = this.width;
 		this.ctx.height = this.canvas.height = this.height;
+		this.ctx.fillStyle = this.bg;
+		this.ctx.fillRect(0, 0, this.width, this.height);
 	}
 }

7c3e4eeb6157d731416229e370366c3a0f4e79b3
Brent21Webb <brent20020@gmail.com>
2019-09-08 13:06:14 +0200

    Added the canvas tag to the canvas constructor

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
-//this comment helps to cl
-var canvas = new Canvas(800, 800, "#55BB55");
+// BASIC FUNCTIONS
+function geti(s) { // getElementById shortcut
+	document.getElementById(s);
+}
+var canvas = new Canvas(800, 800, "#55BB55", geti("map"));

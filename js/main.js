6650dcf580604370a683be3471316edb93650518
Brent21Webb <brent20020@gmail.com>
2019-09-08 13:13:30 +0200

    Fixed geti() --> Added return (stupid me XD)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // BASIC FUNCTIONS
 function geti(s) { // getElementById shortcut
-	document.getElementById(s);
+	return document.getElementById(s);
 }
 var canvas = new Canvas(800, 800, "#55BB55", geti("map"));

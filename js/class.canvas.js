324b8a779c77a79c5362b68d75c77d6929a7b42c
Brent21Webb <brent20020@gmail.com>
2019-09-08 13:07:01 +0200

    Added HMTL element to constructor, initialise function

--- js/class.canvas.js
+++ js/class.canvas.js
@@ -1,5 +1,14 @@
 class Canvas {
-	constructor(width, height, bg) {
-		
+	constructor(width, height, bg, elem) {
+		this.width = width;
+		this.height = height;
+		this.bg = bg;
+		this.elem = elem;
+
+		this.__init();
+	}
+
+	__init() {
+
 	}
 }

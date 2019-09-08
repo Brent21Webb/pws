55a5cc9ab26e5efca21b7c797b2df0bcd4c62d05
Brent21Webb <brent20020@gmail.com>
2019-09-08 13:45:19 +0200

    Added grid system for background

--- js/class.canvas.js
+++ js/class.canvas.js
@@ -13,8 +13,8 @@ class Canvas {
 		this.ctx = this.canvas.getContext("2d");
 		this.ctx.width = this.canvas.width = this.width;
 		this.ctx.height = this.canvas.height = this.height;
-		// Colour the background
-		this.drawBackground();
+		// Draw the background with grid
+		this.drawBackground(true);
 
 		// Set eventlistener for canvas click ==> Draw/select road
 		this.canvas.addEventListener("click", function(e) {
@@ -22,10 +22,27 @@ class Canvas {
 		});
 	} // __init()
 
-	drawBackground() {
+	drawBackground(grid) {
 		this.ctx.fillStyle = this.bg;
 		this.ctx.fillRect(0, 0, this.width, this.height);
-	}
+		if(grid) {
+			this.ctx.strokeStyle = "#444";
+			this.ctx.lineWidth = 1;
+			// Draw horizontal
+			for(var i = 40; i < this.width; i += 40) {
+				this.ctx.moveTo(0.5, i + 0.5);
+				this.ctx.lineTo(this.width, i + 0.5);
+				this.ctx.stroke();
+			}
+			// Draw vertical
+			for(var i = 40; i < this.height; i += 40) {
+				this.ctx.lineStyle = "#444";
+				this.ctx.moveTo(i + 0.5, 0.5);
+				this.ctx.lineTo(i + 0.5, this.height + 0.5);
+				this.ctx.stroke();
+			}
+		} // grid
+	} // drawBackground
 
 	drawRoads() {
 

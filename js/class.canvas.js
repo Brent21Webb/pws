65cf9674bb37f32976b901293589e516d02cf26e
Brent21Webb <brent20020@gmail.com>
2019-09-08 13:08:12 +0200

    Added initialise function

--- js/class.canvas.js
+++ js/class.canvas.js
@@ -3,12 +3,14 @@ class Canvas {
 		this.width = width;
 		this.height = height;
 		this.bg = bg;
-		this.elem = elem;
+		this.canvas = elem;
 
 		this.__init();
 	}
 
 	__init() {
-
+		this.ctx = this.canvas.getContext("2d");
+		this.ctx.width = this.canvas.width = this.width;
+		this.ctx.height = this.canvas.height = this.height;
 	}
 }

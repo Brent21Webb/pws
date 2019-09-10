3fdb613eb1a6a8d7fb57991d5f11de4677e2a82e
Brent21Webb <brent20020@gmail.com>
2019-09-10 12:01:57 +0200

    Finished Segment constructor

--- js/class.segment.js
+++ js/class.segment.js
@@ -1,6 +1,10 @@
 class Segment {
 	//         Vector Vector Int IntArray (ID)
  	constructor(begin, end, speed, connected) {
+		this.begin = begin;
+		this.end = end;
+		this.speed = speed;
+		this.connected = connected;
 		this.__init();
 	} // constructor
 

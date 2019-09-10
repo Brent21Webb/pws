19d200891acf11f8adc045c7613a8c24cfbb996d
Brent21Webb <brent20020@gmail.com>
2019-09-10 12:06:57 +0200

    Changed background colour to match up with the road sprite sides

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function geti(s) { // getElementById shortcut
 }
 
 // Make the canvas element
-var canvas = new Canvas(920, 920, "#77DD77", geti("map"));
+var canvas = new Canvas(920, 920, "#A2C969", geti("map"));
 
 // Make the segments and add them to the canvas
 

1f49986c3445b2097293e063f7e55b92e01ce915
Brent21Webb <brent20020@gmail.com>
2019-09-10 11:50:46 +0200

    Added 'Segment' ID and init functionality

--- js/class.segment.js
+++ js/class.segment.js
@@ -1,8 +1,16 @@
 class Segment {
-	//         Vector Vector Int IntArray (ID) Int
- 	constructor(begin, end, speed, connected, ID) {
-
+	//         Vector Vector Int IntArray (ID)
+ 	constructor(begin, end, speed, connected) {
+		this.__init();
 	} // constructor
+
+	__init() {
+		this.ID = Segment.ID++;
+	}
+
+	draw() {
+
+	}
 } // class Segment
 
 Segment.ID = 1;

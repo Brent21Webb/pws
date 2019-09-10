d769ad66344bd59dbed2662cc7d87f8d067ae1fd
Brent21Webb <brent20020@gmail.com>
2019-09-10 11:54:28 +0200

    Comment layout for what needs to happen

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ function geti(s) { // getElementById shortcut
 	return document.getElementById(s);
 }
 
-// Make the canvas element 
+// Make the canvas element
 var canvas = new Canvas(800, 800, "#77DD77", geti("map"));
+
+// Make the segments and add them to the canvas 

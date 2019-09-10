e8a04e26e2766fd8ab3c657eaa7fe89b4a39a412
Brent21Webb <brent20020@gmail.com>
2019-09-10 12:19:19 +0200

    Added ID console.log

--- js/main.js
+++ js/main.js
@@ -8,4 +8,6 @@ var canvas = new Canvas(920, 920, "#A2C969", geti("map"));
 
 // Make the segments and add them to the canvas
 var seg1 = new Segment(new Vector(1, 1), new Vector(1, 6), 50, true, []);
+
+console.log(seg1.ID);
 // canvas.addSegment(seg1);

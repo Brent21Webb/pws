80497bfefb0bf07d922fa9c99abce4811d740608
Brent21Webb <brent20020@gmail.com>
2019-09-24 12:30:34 +0200

    changed direction comment

--- js/class.segment.js
+++ js/class.segment.js
@@ -4,7 +4,7 @@ class Segment {
 		this.begin = begin;
 		this.end = end;
 		this.speed = speed;
-		this.dir = dir; // 1> Right is up -- 2> Right is right -- 3> Right is down -- 4> Right is left
+		this.dir = dir; // 1> Right goes up -- 2> Right goes left -- 3> Right goes down -- 4> Right goes right
 		this.connected = connected;
 		this.canvas = canvas;
 

3df608bb35ee7dd930dece6f6d43135933c10bfa
Brent21Webb <brent20020@gmail.com>
2019-10-07 15:07:33 +0200

    Changed segment connection array

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ function geti(s) { // getElementById shortcut
 var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 
 // Make the segments and add them to the canvas
-var seg1 = new Segment(new Vector(1, 1), new Vector(1, 6), 50, 1, [1], canvas);
-var seg2 = new Segment(new Vector(3, 6), new Vector(6, 6), 50, 2, [2], canvas);
+var seg1 = new Segment(new Vector(1, 1), new Vector(1, 6), 50, 1, [[2, 1]], canvas);
+var seg2 = new Segment(new Vector(3, 6), new Vector(6, 6), 50, 2, [], canvas);
 
 console.log(seg1.ID);
 console.log(canvas.segments);

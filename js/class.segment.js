1abc06c4fbd29d5fb8293f904731b062eeeacdec
Brent21Webb <brent20020@gmail.com>
2019-10-07 17:59:07 +0200

    Removed unnecessary whitelines, comments and console.logs

--- js/class.segment.js
+++ js/class.segment.js
@@ -48,12 +48,13 @@ class Segment {
 	} // __init()
 
 	draw(ctx) {
-		console.log(this.dx + " - " + this.dy + " (" + this.dir + ")");
-
+		// Calculate the correction and maximum
 		const corr = (this.dir >= 3 ? -1 : 1);
 		var max = (this.dx || this.dy) * corr;
 
+		// For every piece of road
 		for(var i = 0; (corr > 0 ? (i < max) : (i > max)); i += corr) {
+			// Calculate x and y position (either fixed position, or i + the beginning - correction)
 			var x = (this.dx === 0 ? this.begin.x * 30 : (i + this.begin.x) * 30 - (corr < 0 ? 30 : 0));
 			var y = (this.dy === 0 ? this.begin.y * 30 : (i + this.begin.y) * 30 - (corr < 0 ? 30 : 0));
 
@@ -61,9 +62,7 @@ class Segment {
 			var w = (this.dir % 2 === 1 ? 60 : 30);
 			var h = (this.dir % 2 === 0 ? 60 : 30);
 
-
 			ctx.drawImage(this.sprite, x, y, w, h);
-
 
 			// White square on end vector
 			ctx.fillStyle = "#FFF";
@@ -76,31 +75,6 @@ class Segment {
 		for(var i in this.connected) {
 			ctx.drawImage(Segment.CONNECTIONS[this.connected[i][1]], this.end.x * 30, this.end.y * 30, 60, 60);
 		} // for i in connected
-
-
-
-		// var t = Math.max(this.dx, this.dy);
-		// for(var i = 0; i < t; i++) {
-		// 	// Calculate for every piece of the length of the road where it should be located
-		// 	var x = (this.dx === 0 ? this.begin.x * 30 : (i + this.begin.x) * 30);
-		// 	var y = (this.dy === 0 ? this.begin.y * 30 : (i + this.begin.y) * 30);
-		// 	// Calculate width and height (horz/vert)
-		// 	var w = (this.dir % 2 === 1 ? 60 : 30);
-		// 	var h = (this.dir % 2 === 0 ? 60 : 30);
-		// 	ctx.drawImage(this.sprite, x, y, w, h);
-		//
-		// 	// White square on end vector
-		// 	ctx.fillStyle = "#FFF";
-		// 	var tx = this.end.x * 30 + (25 * (this.dir % 2)) - (10 * (1 - this.dir % 2));
-		// 	var ty = this.end.y * 30 + (25 * (1 - this.dir % 2)) - (10 * (this.dir % 2));
-		// 	ctx.fillRect(tx, ty, 10, 10);
-		// }
-		//
-		// // Draw the road connections
-		// for(var i in this.connected) {
-		// 	ctx.drawImage(Segment.CONNECTIONS[this.connected[i][1]], this.end.x * 30, this.end.y * 30, 60, 60);
-		// } // for i in connected
-
 
 	} // draw(ctx)
 } // class Segment

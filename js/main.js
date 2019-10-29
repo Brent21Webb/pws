6a3f57ef62e305d372b05e2562feaf3977246db2
Brent21Webb <brent20020@gmail.com>
2019-10-29 11:52:24 +0100

    Removed testing segments

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 
 // Make the segments and add them to the canvas
 var seg1 = new Segment(new Vector(1, 1), new Vector(1, 6), 50, [[2, 1]], canvas);
-var seg2 = new Segment(new Vector(15, 6), new Vector(15, 1), 50, [], canvas);
-// var seg3 = new Segment(new Vector(3, 6), new Vector(6, 6), 50, [], canvas);
+// var seg2 = new Segment(new Vector(15, 6), new Vector(15, 1), 50, [], canvas);
+var seg3 = new Segment(new Vector(3, 6), new Vector(6, 6), 50, [], canvas);
 // var seg4 = new Segment(new Vector(6, 8), new Vector(3, 8), 50, [], canvas);
 
 // console.log(seg1.ID);

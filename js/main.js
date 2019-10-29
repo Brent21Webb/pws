225144462d2e7aba381b830e9dd7e252eaf1d82a
Brent21Webb <brent20020@gmail.com>
2019-10-29 12:21:31 +0100

    Edited connector in constructor to fit new connecting programmign

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function geti(s) { // getElementById shortcut
 var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 
 // Make the segments and add them to the canvas
-var seg1 = new Segment(new Vector(1, 1), new Vector(1, 6), 50, [[2, 1]], canvas);
+var seg1 = new Segment(new Vector(1, 1), new Vector(1, 6), 50, [2, [1]], canvas);
 // var seg2 = new Segment(new Vector(15, 6), new Vector(15, 1), 50, [], canvas);
 var seg3 = new Segment(new Vector(3, 6), new Vector(10, 6), 50, [], canvas);
 // var seg4 = new Segment(new Vector(6, 8), new Vector(3, 8), 50, [], canvas);

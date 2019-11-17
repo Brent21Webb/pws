6de5bb9dddfa2e62f73f97b796070773c0e41f68
Brent21Webb <brent20020@gmail.com>
2019-11-17 14:39:27 +0100

    Added constructor, sprite initialisation

--- js/class.vehicle.js
+++ js/class.vehicle.js
@@ -1,5 +1,30 @@
 class Vehicle {
-	constructor() {
-		
+	constructor(segment, end) {
+		this.segment = segment;
+		this.end = end;
+		this.sprite = undefined;
+
+		this.__init();
+	}
+
+	__init() {
+		// SPRITE LOADING
+		// this.sprite = new Image();
+		// this.sprite.src = (this.dir % 2 === 0 ? "sprites/roads/roadHalfHorizontalNew.png" : "sprites/roads/roadHalfNew.png");
+
+		// Load sprite images
+		this.CARS = ["blue", "green", "orange", "pink"];
+		for(var i in this.CARS) {
+			var TEMP_IMG = new Image();
+			TEMP_IMG.src = "sprites/cars/" + this.CARS[i] + "CarPolished.png";
+			Vehicle.SPRITES[i] = TEMP_IMG;
+		}
+
+		// Set sprite based on end point
+		if(this.end == 1) {
+			this.sprite = new Image();
+			this.sprite.src = "";
+		}
 	}
 }
+Vehicle.SPRITES = [];

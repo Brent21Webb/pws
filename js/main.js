22c0804835923b9badf10810ec95df05f9e67ea0
Brent21Webb <brent20020@gmail.com>
2019-12-03 13:00:04 +0100

    Added some segments, created some bugs

--- js/main.js
+++ js/main.js
@@ -10,11 +10,11 @@ function timeDiff(t1, t2) {
 var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 
 // Make the segments and add them to the canvas
-var seg1 = new Segment(new Vector(1, 1), new Vector(1, 6), 50, [1, [2]], canvas);
-// var seg2 = new Segment(new Vector(15, 6), new Vector(15, 1), 50, [], canvas);
-var seg3 = new Segment(new Vector(3, 6), new Vector(10, 6), 50, [3, [3]], canvas);
-var seg4 = new Segment(new Vector(10, 8), new Vector(10, 11), 50, [], canvas)
-// var seg4 = new Segment(new Vector(6, 8), new Vector(3, 8), 50, [], canvas);
+var seg1 = new Segment(new Vector(10, 1), new Vector(10, 6), 50, [1, [2]], canvas);
+var seg2 = new Segment(new Vector(12, 6), new Vector(15, 6), 50, [3, [3]], canvas);
+var seg3 = new Segment(new Vector(15, 8), new Vector(15, 11), 50, [4, [4]], canvas)
+var seg4 = new Segment(new Vector(15, 11), new Vector(5, 11), 50, [2, [5]], canvas);
+var seg5 = new Segment(new Vector(5, 11), new Vector(5, 3), 50, [], canvas);
 
 
 // TODO: make a segment with multiple exit points

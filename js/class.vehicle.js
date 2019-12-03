376cf5d84758bc4c21ce905a0a613301ab7ff2d6
Brent21Webb <brent20020@gmail.com>
2019-12-03 12:42:08 +0100

    Changed sprite choosing moment, fixed speed, fixed update function

--- js/class.vehicle.js
+++ js/class.vehicle.js
@@ -22,23 +22,22 @@ class Vehicle {
 			TEMP_IMG.src = "sprites/cars/" + this.colour + "/" + this.DIRECTIONS[i] + ".png";
 			Vehicle.SPRITES[i] = TEMP_IMG;
 		}
-
-		// Set sprite based on segment direction
-		this.sprite = Vehicle.SPRITES[this.segment.dir - 1];
 	}
 
 	update() {
-		this.x += this.segment.dx * (25 / this.segment.speed);
-		this.y += this.segment.dy * (25 / this.segment.speed);
+		this.x += (this.segment.dx ? this.segment.speed / 25 : 0);
+		this.y += (this.segment.dy ? this.segment.speed / 25 : 0);
 
 		// TODO: if car exceeds the segment length (including the connector, so length + 1 segment part (= 60px))
-		if(this.x > (this.segment.end.x + 1) * 30 || this.y > (this.segment.end.y + 1) * 30) {
+		if((this.x > (this.segment.end.x + 1) * 30 && this.segment.dx !== 0) || (this.y > (this.segment.end.y + 1) * 30 && this.segment.dy !== 0)) {
 			this.segment = this.canvas.segments[this.segment.connected[1][0] - 1]; // Equal to next segment --> Either the only one or the one which leads to the shortest route to the end
-			console.log(this.segment)
+			console.log(this.segment);
 		}
 	} // update()
 
 	draw(ctx) {
+		// Set sprite based on segment direction
+		this.sprite = Vehicle.SPRITES[this.segment.dir - 1];
 		var w, h;
 		switch(this.segment.dir) {
 			case 1: // If goes up or down

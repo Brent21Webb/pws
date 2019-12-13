1cb065ee0f56613fb73d99e1c66c1cba1cb29659
Brent21Webb <brent20020@gmail.com>
2019-12-13 17:38:10 +0100

    Added a new endpoint

--- js/main.js
+++ js/main.js
@@ -12,9 +12,10 @@ var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 // Make the segments and add them to the canvas
 var seg1 = new Segment(new Vector(10, 1), new Vector(10, 6), 50, [1, [2]], canvas);
 var seg2 = new Segment(new Vector(12, 6), new Vector(15, 6), 50, [3, [3]], canvas);
-var seg3 = new Segment(new Vector(15, 8), new Vector(15, 11), 50, [4, [4]], canvas);
+var seg3 = new Segment(new Vector(15, 8), new Vector(15, 11), 50, [4, [4, 6]], canvas);
 var seg4 = new Segment(new Vector(15, 11), new Vector(5, 11), 50, [1, [5]], canvas);
 var seg5 = new Segment(new Vector(5, 11), new Vector(5, 3), 50, [], canvas);
+var seg6 = new Segment(new Vector(15, 11), new Vector(15, 16), 50, [], canvas);
 
 
 // TODO: vehicles disappear for 1 frame when new vehicle spawns

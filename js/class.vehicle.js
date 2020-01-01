e57ec4440d84bb1b2cf8d16c0deb85576db8bed2
Brent21Webb <brent20020@gmail.com>
2020-01-01 13:03:37 +0100

    Added new segment selection procedure

--- js/class.vehicle.js
+++ js/class.vehicle.js
@@ -38,22 +38,25 @@ class Vehicle {
 			if(this.segment.endpoint) {
 				this.destroySelf();
 			}
-			this.segment = this.nextSegment; // Make its new segment the next segment...
-	
-			// ...and find the new nextSegment
-			var thisConnected = this.segment.connected[1]; 
-			if(thisConnected && this.canvas.segments[thisConnected[0] - 1]) {
-				this.nextSegment = this.canvas.segments[thisConnected[0] - 1];
-			}
-			
-			// Calculate dx/dy for positioning the vehicle after the segment change
-			let dx = 15 * (this.segment.dir % 2 === 0 ? 0 : 1);
-			let dy = 15 * (this.segment.dir % 2 === 1 ? 0 : 1);
 
-			// Forced is used for the negative x/y drawing values 
+			var olddir = this.segment.dir
+			this.segment = this.nextSegment; // Make its new segment the next segment...
+
+			// ...and find the new nextSegment
+			var thisConnected = this.segment.connected[1];
+			if(thisConnected && this.canvas.segments[thisConnected[0] - 1]) {
+				var n = thisConnected.length - 1; n = 0;
+				this.nextSegment = this.canvas.segments[thisConnected[n] - 1];
+			}
+
+			// Calculate dx/dy for positioning the vehicle after the segment change
+			let dx = 15 * (this.segment.dir % 2 === 0 ? 0 : 1) * (olddir == this.segment.dir ? 0 : 1);
+			let dy = 15 * (this.segment.dir % 2 === 1 ? 0 : 1) * (olddir == this.segment.dir ? 0 : 1);
+
+			// Forced is used for the negative x/y drawing values
 			let forcedDX = 20 * (this.segment.dir === 4 ? -1 : 0);
 			let forcedDY = 30 * (this.segment.dir === 3 ? -1 : 0);
-			
+
 			// Adjust vehicle up/down/right/left to drive in the center of the road, based on the calculations made before the segment changed
 			this.x += dx + forcedDX;
 			this.y += dy + forcedDY;

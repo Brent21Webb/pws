16baffb3051e14076597c184e66a50a2f1c96493
Brent21Webb <brent20020@gmail.com>
2020-01-01 13:15:52 +0100

    Set all segments to visible

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ function timeDiff(t1, t2) {
 var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 
 // Make the segments and add them to the canvas
-var seg1 = new Segment(new Vector(10, 1), new Vector(10, 6), 50, [1, [2]], canvas);
-var seg2 = new Segment(new Vector(12, 6), new Vector(15, 6), 50, [3, [3]], canvas);
-var seg3 = new Segment(new Vector(15, 8), new Vector(15, 11), 50, [0, [4, 6]], canvas);
-var seg4 = new Segment(new Vector(15, 11), new Vector(5, 11), 50, [1, [5]], canvas);
-var seg5 = new Segment(new Vector(5, 11), new Vector(5, 3), 50, [], canvas);
-var seg6 = new Segment(new Vector(15, 13), new Vector(15, 16), 50, [], canvas);
+var seg1 = new Segment(new Vector(10, 1), new Vector(10, 6), 50, [1, [2]], true, canvas);
+var seg2 = new Segment(new Vector(12, 6), new Vector(15, 6), 50, [3, [3]], true, canvas);
+var seg3 = new Segment(new Vector(15, 8), new Vector(15, 11), 50, [0, [4, 6]], true, canvas);
+var seg4 = new Segment(new Vector(15, 11), new Vector(5, 11), 50, [1, [5]], true, canvas);
+var seg5 = new Segment(new Vector(5, 11), new Vector(5, 3), 50, [], true, canvas);
+var seg6 = new Segment(new Vector(15, 13), new Vector(15, 16), 50, [], true, canvas);
 
 
 // TODO: vehicles disappear for 1 frame when new vehicle spawns

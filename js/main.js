d1bd10373bf0aa8146181292d454334732424c9f
Brent21Webb <brent20020@gmail.com>
2020-01-02 13:49:19 +0100

    Added some segments (and commented segments) for the actual situation

--- js/main.js
+++ js/main.js
@@ -18,6 +18,20 @@ var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 // var seg6 = new Segment(new Vector(15, 13), new Vector(15, 16), 50, [], true, canvas);
 
 
+var s1 = new Segment(new Vector(28, 2), new Vector(2, 2), 50, [2, [3]], true, canvas);
+var s2 = new Segment(new Vector(8, 4), new Vector(28, 4), 50, [5, []], true, canvas);
+var s3 = new Segment(new Vector(2, 4), new Vector(2, 28), 50, [5, []], true, canvas);
+var s4 = new Segment(new Vector(6, 28), new Vector(6, 4), 50, [2, [2]], true, canvas);
+// var s5 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+// var s6 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+// var s7 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+// var s8 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+// var s9 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+// var s10 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+// var s11 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+// var s12 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
+
+
 // TODO: vehicles disappear for 1 frame when new vehicle spawns
 // TODO: make a segment with multiple exit points => Done, now get them to move in different directions
 
@@ -25,5 +39,21 @@ canvas.initSegmentSpawning();
 canvas.initEndpoints();
 
 // console.log(seg1.ID);
+console.info("Segments");
 console.log(canvas.segments);
+console.log("");
+
+console.info("Spawners");
+console.log(canvas.spawners);
+console.log("");
+
+console.info("Endings");
+console.log(canvas.endpoints);
+console.log("");
+
+
 // canvas.addSegment(seg1);
+
+
+
+

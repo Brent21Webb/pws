967a7114c73c6ea73ce18954568ee32390bb9045
Brent21Webb <brent20020@gmail.com>
2020-01-17 15:03:59 +0100

    Forced one vehicle spawned at ID 7

--- js/class.segment.js
+++ js/class.segment.js
@@ -58,8 +58,8 @@ class Segment {
 
 
 	spawn(x) {
-		// if(x && !this.canvas.vehicles[0]) {
-		if(x > 3) {
+		if(x && !this.canvas.vehicles[0] && this.ID === 7) {
+		// if(x > 3) {
 			var v = new Vehicle(this.canvas, this);
 			this.canvas.vehicles.push(v);
 			console.log(v);

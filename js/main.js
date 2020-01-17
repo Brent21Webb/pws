b856ed9a1de754d789ec4f8dff321ba53e32d744
Brent21Webb <brent20020@gmail.com>
2020-01-17 14:23:18 +0100

    trying to create a new road layout

--- js/main.js
+++ js/main.js
@@ -9,19 +9,15 @@ function timeDiff(t1, t2) {
 // Make the canvas element
 var canvas = new Canvas(930, 930, "#A2C969", geti("map"));
 
-// Make the segments and add them to the canvas
-// var seg1 = new Segment(new Vector(10, 1), new Vector(10, 6), 50, [1, [2]], true, canvas);
-// var seg2 = new Segment(new Vector(12, 6), new Vector(15, 6), 50, [3, [3]], true, canvas);
-// var seg3 = new Segment(new Vector(15, 8), new Vector(15, 11), 50, [0, [4, 6]], true, canvas);
-// var seg4 = new Segment(new Vector(15, 11), new Vector(5, 11), 50, [1, [5]], true, canvas);
-// var seg5 = new Segment(new Vector(5, 11), new Vector(5, 3), 50, [], true, canvas);
-// var seg6 = new Segment(new Vector(15, 13), new Vector(15, 16), 50, [], true, canvas);
+var s1 = new Segment(new Vector(2, 2), new Vector(2, 16), 50, [1, [2]], true, canvas);
+var s2 = new Segment(new Vector(4, 16), new Vector(16, 16), 50, [0, []], true, canvas);
+var s3 = new Segment(new Vector(2, 4), new Vector(2, 16), 50, [3, [4]], true, canvas);
+var s4 = new Segment(new Vector(4, 16), new Vector(14, 16), 50, [6, []], true, canvas);
 
-
-var s1 = new Segment(new Vector(28, 2), new Vector(2, 2), 50, [2, [3]], true, canvas);
-var s2 = new Segment(new Vector(8, 4), new Vector(28, 4), 50, [5, []], true, canvas);
-var s3 = new Segment(new Vector(2, 4), new Vector(2, 28), 50, [5, []], true, canvas);
-var s4 = new Segment(new Vector(6, 28), new Vector(6, 4), 50, [2, [2]], true, canvas);
+// var s1 = new Segment(new Vector(28, 2), new Vector(2, 2), 50, [2, [3]], true, canvas);
+// var s2 = new Segment(new Vector(8, 4), new Vector(28, 4), 50, [5, []], true, canvas);
+// var s3 = new Segment(new Vector(2, 4), new Vector(2, 28), 50, [5, []], true, canvas);
+// var s4 = new Segment(new Vector(6, 28), new Vector(6, 4), 50, [2, [2]], true, canvas);
 // var s5 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
 // var s6 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);
 // var s7 = new Segment(new Vector(), new Vector(), speed, [contype, conn], true, canvas);

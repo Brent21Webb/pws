4e20d214cc581d0393e6f54286215d55cb34a7c8
Brent21Webb <brent20020@gmail.com>
2020-01-28 12:37:54 +0100

    Changed spawner

--- js/class.segment.js
+++ js/class.segment.js
@@ -58,7 +58,7 @@ class Segment {
 
 
 	spawn(x) {
-		if(x && !this.canvas.vehicles[0] && this.ID === 9) {
+		if(x && !this.canvas.vehicles[0] && this.ID === 10) {
 		// if(x > 3) {
 			var v = new Vehicle(this.canvas, this);
 			this.canvas.vehicles.push(v);

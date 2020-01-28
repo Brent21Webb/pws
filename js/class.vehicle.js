4473934a266bf58601af9fb80ec1072abe8379d2
Brent21Webb <brent20020@gmail.com>
2020-01-28 12:41:07 +0100

    Fixed issue where car would turn too late when going upwards

--- js/class.vehicle.js
+++ js/class.vehicle.js
@@ -47,7 +47,7 @@ class Vehicle {
 
 	update() {
 		let ex = this.segment.end.x;
-		let ey = this.segment.end.y + (this.segment.dir === 3 ? -2 : 0);
+		let ey = this.segment.end.y + (this.segment.dir === 3 ? -2 : 0) * (this.segment.connected[0] === 6 ? 1 : 0);
 		let pastpointCorr = (this.segment.dir >= 3 ? -1 : 1);
 
 		html.innerHTML = "End: " + ex + " - " + ey + " (" + ex * 30 + " - " + ey * 30 + ")<br>";

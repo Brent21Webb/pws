0a4ec42a6fdb753e0d75a3b844c4d81d3aa3aa4c
Brent21Webb <brent20020@gmail.com>
2020-02-09 13:45:27 +0100

    Set spawning rate back to 60%

--- js/class.segment.js
+++ js/class.segment.js
@@ -61,7 +61,7 @@ class Segment {
 
 	spawn(x) {
 		// if(x && !this.canvas.vehicles[0] && (this.ID === 1 || this.ID === 1)) {
-		if(x >= 0) {
+		if(x > 3) {
 			var v = new Vehicle(this.canvas, this);
 			this.canvas.vehicles.push(v);
 		}
